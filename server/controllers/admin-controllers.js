bc19828028ddbf550068341b3e8e1ea821b6dc87
Gautam Kumar <91417015+gautamkumar1@users.noreply.github.com>
2024-04-04 16:15:58 +0530

    Retrieving Contacts Data From MongoDB

--- server/controllers/admin-controllers.js
+++ server/controllers/admin-controllers.js
@@ -1,16 +1,29 @@
-const User = require('../models/user-model')
-const getAllUsers = async (req,res) =>{
-    try {
-        const users = await User.find({},{password:0});
-        if(!users || users.length === 0){
-            // handle the case where no documents was found
-            return res.status(404).json({message:"No users found"})
-           
-        }
-       return res.status(200).json(users);
-    } catch (error) {
-        next(error)
+const User = require("../models/user-model");
+const Contact = require("../models/contact-model")
+const getAllUsers = async (req, res,next) => {
+  try {
+    const users = await User.find({}, { password: 0 });
+    if (!users || users.length === 0) {
+      // handle the case where no documents was found
+      return res.status(404).json({ message: "No users found" });
     }
-}
+    return res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = getAllUsers
+const getAllContacts = async (req, res,next) => {
+  try {
+    const contacts = await Contact.find();
+    // console.log(contacts);
+    if (!contacts || contacts.length === 0) {
+      // handle the case where no contact was found
+      return res.status(404).json({ message: "No contacts found" });
+    }
+    return res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {getAllUsers,getAllContacts};
--- server/routes/admin-route.js
+++ server/routes/admin-route.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const getAllUsers = require('../controllers/admin-controllers');
+const {getAllUsers,getAllContacts} = require('../controllers/admin-controllers');
 const router = express.Router();
 router.route("/users").get(getAllUsers)
-
+router.route("/contacts").get(getAllContacts);
 module.exports = router;
