2c8c7effd87f632b714a4dc5fb6fb98258334f7f
Gautam Kumar <91417015+gautamkumar1@users.noreply.github.com>
2024-04-04 22:13:48 +0530

    Admin Middleware Added

--- server/routes/admin-route.js
+++ server/routes/admin-route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {getAllUsers,getAllContacts} = require('../controllers/admin-controllers');
 const authMiddleware = require('../middlewares/auth-midlleware')
+const adminMiddleware = require('../middlewares/admin-middleware');
 const router = express.Router();
-router.route("/users").get(authMiddleware,getAllUsers);
-router.route("/contacts").get(authMiddleware,getAllContacts);
+router.route("/users").get(authMiddleware, adminMiddleware,getAllUsers);
+router.route("/contacts").get(authMiddleware, adminMiddleware,getAllContacts);
 module.exports = router;
